b00d1f2330d955fee59e0e40f91befe106bac86b
NicoleMcS <74243549+NicoleMcS@users.noreply.github.com>
2020-11-25 09:50:20 +0000

    Create fnctions.js

---
+++ module-3/scripts/fnctions.js
@@ -0,0 +1,22 @@
+/*  
+Declaring a function
+
+
+To declare a function, you use the function keyword, followed by the function name, a list of parameters, and the function bdy.
+
+(e.g. functionAdd)
+function functionName( parameters ) {
+
+    function body
+
+    CODE GOES HERE
+
+}
+
+*/
+
+function greeting( name ) {
+
+    console.log( name );
+    
+}
