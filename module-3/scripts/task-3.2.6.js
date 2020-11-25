1a89fc8a17c408971bb0c30fb28d5174d47fe4ec
NicoleMcS <74243549+NicoleMcS@users.noreply.github.com>
2020-11-25 13:50:21 +0000

    Create task-3.2.6.js

---
+++ module-3/scripts/task-3.2.6.js
@@ -0,0 +1,18 @@
+var temperature = 10;
+
+if ( temperature < 0 ) {
+
+    console.log('Stay inside');
+    
+    } else if ( temperature < 30 ) {
+
+    console.log('Wear a coat and hat');
+        
+     } else if (temperature < 50) {
+
+    console.log('Put a coat on');
+
+} else {
+
+    console.log( 'It is fine to just wear pants and vest' );
+}
