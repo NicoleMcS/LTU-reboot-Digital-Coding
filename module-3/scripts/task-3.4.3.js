26ad4d1f30828598612868738caecbdfe06098b1
NicoleMcS <74243549+NicoleMcS@users.noreply.github.com>
2020-11-26 10:51:43 +0000

    Create task-3.4.3.js
    favourite foods to list to page

---
+++ module-3/scripts/task-3.4.3.js
@@ -0,0 +1,7 @@
+var favouriteFoods = ['Lasagne' , 'Curry' , 'Cookie  Dough Ice cream' , 'Speculoos']
+
+for(var index = 0; ,favouriteFoods.length; index++ {
+    
+    document.write('<h1>' + favouriteFoods[index] + '</h1>')
+
+})
