965ff041b0a97fa30037677523d8a3051cd2ca40
NicoleMcS <74243549+NicoleMcS@users.noreply.github.com>
2020-11-27 10:20:02 +0000

    DOM
    Explaining how DOM works

---
+++ module-3/scripts/DOM.js
@@ -0,0 +1,11 @@
+let heading1 = document.querySelector('h1');
+
+console.log(heading1 );
+
+let paragraph = document.querySelector('p');
+
+console.log( paragraph ); 
+
+let listItem2 = documet.querySelectorAll('li');
+
+console.log(listItems[1]);
